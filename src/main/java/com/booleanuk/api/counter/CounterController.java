21290e48d28fe7ae1b16a31c0b4da2b1e8264042
Dave Ames <david.john.ames@gmail.com>
2023-08-09 15:42:54 +0100

    Added the custom named counter endpoint

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,30 +1,46 @@
 package com.booleanuk.api.counter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
+@RequestMapping("/counter")
 public class CounterController {
     private int count;
+    private Map<String, Integer> counters;
 
     public CounterController() {
         this.count = 0;
+        this.counters = new HashMap<>();
     }
 
-    @GetMapping("/counter")
+    @GetMapping
     public int getCount() {
         return this.count;
     }
 
-    @GetMapping("/counter/increment")
+    @GetMapping("/increment")
     public int increment() {
         this.count++;
         return this.count;
     }
 
-    @GetMapping("/counter/decrement")
+    @GetMapping("/decrement")
     public int decrement() {
         this.count--;
         return this.count;
+    }
+
+    @GetMapping("/{name}")
+    public int getNamedCounter(@PathVariable String name) {
+        if (!this.counters.containsKey(name)) {
+            this.counters.put(name, 0);
+        }
+        return this.counters.get(name);
     }
 }
