18abfd10ddd6927b28a8718667035f54247674b9
Dave Ames <david.john.ames@gmail.com>
2023-08-09 15:21:43 +0100

    Annotated the class and added a get method

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,18 @@
 package com.booleanuk.api.counter;
 
-public class CounterController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class CounterController {
+    private int count;
+
+    public CounterController() {
+        this.count = 0;
+    }
+
+    @GetMapping("/counter")
+    public int getCount() {
+        return this.count;
+    }
 }
